544685f2001402882106586cfbdd8881d8d22e2d
jabruce56 <jabruce56@aol.com>
2016-11-28 15:51:52 -0800

    level 1 working without proper processes

--- initfs.c
+++ initfs.c
@@ -1,36 +1,36 @@
 #include "type.h"
 #include "include.h"
-
+int nproc=0;
 int init(){ // Initialize data structures of LEVEL-1:
      int i, j;
      PROC *p;
      //reset global proc and minode
      for(i=0;i<NPROC;i++){
-       //if(i==0||i==1){proc[i].uid=i;}
        p=&proc[i];
        p->status=FREE;
-       for(j=0;j<NFD;j++){
+       for(j=0;j<NFD;j++)
          p->fd[j]=0;
-         p->cwd=0;
-       }
+       p->next=&proc[i+1];
      }
      for(i=0;i<NMINODE;i++){minode[i].refCount=0;}
+     for(i=0;i<NMOUNT;i++){mounttab[i].busy=0;}
      for(i=0;i<NOFT;i++){oft[i].refCount=0;}
-
-     //2 PROCs, P0 with uid=0, P1 with uid=1, all PROC.cwd = 0
-     //(3). MINODE *root = 0;
      printf("mounting root..\n");
      mount_root();
      printf("mount complete\n");
+
      printf("starting P0\n");
-     running = &proc[0];
      p = &proc[0];
+     running=&proc[0];
      p->status = RUNNING;
      p->next = &proc[1];
      p->uid = 0;
-     p->pid = 1;
+     p->pid = 0;
      p->ppid = 0;
      p->gid = 0;
+     p->parent = p;
+     p->sibling = p;
+     p->child=0;
      p->cwd = root;
      p->cwd->refCount++;
 
@@ -38,22 +38,14 @@ int init(){ // Initialize data structures of LEVEL-1:
      p = &proc[1];
      p->next = &proc[0];
      p->status = RUNNING;
-     p->uid = 1;
+     p->uid = 2;
      p->pid = 1;
      p->ppid = 0;
      p->gid = 0;
      p->cwd = root;
      p->cwd->refCount++;
-
-     //nproc = 2;
+     nproc=2;
 }
-//
-// 4.. Write C code for
-//    Write C code for
-//          int ino     = getino(int *dev, char *pathname)
-//          MINODE *mip = iget(dev, ino)
-//
-//                        iput(MINDOE *mip)
 int mount_root(){  // mount root file system, establish / and CWDs
   int i, ino, fd;
   SUPER *sp;
@@ -104,71 +96,116 @@ int mount_root(){  // mount root file system, establish / and CWDs
   proc[0].cwd=iget(dev,2);
   proc[1].cwd=iget(dev,2);
   printf("root mounted\n");
-  //root = iget(dev, 2);    /* get root inode */
-//
-//  Let cwd of both P0 and P1 point at the root minode (refCount=3)
-//      P0.cwd = iget(dev, 2);
-//      P1.cwd = iget(dev, 2);
 }
 
 
-ls (char *pathname)
-{
-  int i, ino, dev = running->cwd->dev;
-  MINODE *mip = running->cwd;
 
-  if (pathname[0]!='\0'){   // ls pathname:
 
-    pathname[strlen(pathname)-1]='\0';
-    printf("pathname=%s\n", pathname);
-    if (pathname[0]=='/')
-
-    ino = getino(&dev, pathname);
-    mip = iget(dev, ino);
-  }
-  dir(mip->INODE);
-  // mip points at minode;
-  // Each data block of mip->INODE contains DIR entries
-  // print the name strings of the DIR entries
-}
-//
-// 7. cd(char *pathname)
-//    {
-//       if (no pathname)
-//          cd to root;
-//       else
-//          cd to pathname;
-//    }
-//
 int main()
   {
-    char line[256];
-    //printf("%d\n", getino(2, "a/b");
+    char line[256], cname[128], path[128];
+    int cmd;
     init();
-    printf("proc[0].pid=%d\nproc[0].status=%d\nproc[0].uid=%d\nproc[0].",proc[0].pid,proc[0].status,proc[0].uid);
+    // printf("proc[0].pid=%d\nproc[0].status=%d\nproc[0].uid=%d\nproc[0].",proc[0].pid,proc[0].status,proc[0].uid);
     printf("dev=%d\n",dev);
     printf("init complete\n");
     while(1){
+      memset(line, '\0', 256);
+      memset(cname, '\0', 128);
+      memset(path, '\0', 128);
       printf("command: ");
-      fgets(line, sizeof(line), stdin);
-      if(!strncmp(line, "ls", 2)){
-        ls(&line[3]);
+      fgets(line, 256, stdin);
+      sscanf(line, "%s %s", cname, path);
+      if(!strncmp(cname, "mkdir", 5))
+        mk_dir(path);
+      else if(!strncmp(cname, "cd", 2))
+        ch_dir(path);
+      else if(!strncmp(cname, "pwd", 3))
+        pwd(running->cwd);
+      else if(!strncmp(cname, "ls", 2))
+        listdir(path);
+        // else if(!strncmp(cname, "mount", 5))
+        //   return 4;
+        // else if(!strncmp(cname, "umount", 6))
+        //   return 5;
+        // else if(!strncmp(cname, "creat", 5))
+        //   return 6;
+        // else if(!strncmp(cname, "rmdir", 5))
+        //   return 7;
+        // else if(!strncmp(cname, "rm", 2))
+        //   return 8;
+        // else if(!strncmp(cname, "open", 4))
+        //   return 9;
+        // else if(!strncmp(cname, "close", 5))
+        //   return 10;
+        // else if(!strncmp(cname, "read", 4))
+        //   return 11;
+        // else if(!strncmp(cname, "write", 5))
+        //   return 12;
+        // else if(!strncmp(cname, "cat", 3))
+        //   return 13;
+        // else if(!strncmp(cname, "cp", 2))
+        //   return 14;
+        // else if(!strncmp(cname, "mv", 2))
+        //   return 15;
+        // else if(!strncmp(cname, "pfd", 3))
+        //   return 16;
+        // else if(!strncmp(cname, "lseek", 5))
+        //   return 17;
+        // else if(!strncmp(cname, "rewind", 6))
+        //   return 18;
+        // else if(!strncmp(cname, "stat", 4))
+        //   return 19;
+        // else if(!strncmp(cname, "pm", 2))
+        //   return 20;
+        // else if(!strncmp(cname, "menu", 4))
+        //   return 21;
+        // else if(!strncmp(cname, "access", 6))
+        //   return 22;
+        // else if(!strncmp(cname, "chmod", 5))
+        //   return 23;
+        // else if(!strncmp(cname, "chown", 5))
+        //   return 24;
+        // else if(!strncmp(cname, "cs", 2))
+        //   return 25;
+        // else if(!strncmp(cname, "fork", 4))
+        //   return 26;
+        // else if(!strncmp(cname, "ps", 2))
+        //   return 27;
+        // else if(!strncmp(cname, "kill", 4))
+        //   return 28;
+        else if(!strncmp(cname, "quit", 4))
+          quit();
+        // else if(!strncmp(cname, "touch", 5))
+        //   return 30;
+        // else if(!strncmp(cname, "sync", 4))
+        //   return 31;
+        // else if(!strncmp(cname, "link", 4))
+        //   return 32;
+        // else if(!strncmp(cname, "unlink", 6))
+        //   return 33;
+        // else if(!strncmp(cname, "symlink", 7))
+        //   return 34;
+        else
+          printf("error: unknown command");
       }
     }
-//      // ask for a command string, e.g. ls pathname
-//      ls(pathname);
-//      // ask for a command string, e.g. cd pathname
-//      cd(pathname);
-//      // ask for a command string, e.g. stat pathname
-//      stat(pathname, &mystat); // struct stat mystat; print mystat information
-  }
-//
+
+
+
 int quit()
 {
 //      iput all DIRTY minodes before shutdown
   int i;
+  srand(time(NULL));
+  if(rand()%5==0)
+    printf("cleaning up all those dirty inodes ;)...\n");
+  else
+    printf("closing file system...\n");
+  usleep(650);
   for(i=0;i<NMINODE;i++)
     if(minode[i].dirty)
       iput(&minode[i]);
+  printf("complete\n");
   exit(0);
 }
--- util.c
+++ util.c
@@ -1,5 +1,4 @@
 //#include "type.h"
-//int fd;
 char *name[64];
 
 int get_block(int fd, int blk, char buf[ ]){
@@ -14,7 +13,7 @@ int tokenize(char *pathname){
   char *token, path[128], names[64][64];
   int i = 0, j;
   strcpy(path, pathname);
-  printf("tokenizepath=%s\n", path);
+  //printf("tokenizepath=%s\n", path);
   token = strtok(path, "/");
   while(token!=NULL&&i<64){
     strcpy(names[i], token);
@@ -24,33 +23,30 @@ int tokenize(char *pathname){
   }
   return i;
 }
-              //http://man7.org/linux/man-pages/man3/basename.3.html
-              // path       dirname   basename
-              // /usr/lib   /usr      lib
-              // /usr/      /         usr
-              // usr        .         usr
-              // /          /         /
-              // .          .         .
-              // ..         .         ..
+//http://man7.org/linux/man-pages/man3/basename.3.html
+// path       dirname   basename
+// /usr/lib   /usr      lib
+// /usr/      /         usr
+// usr        .         usr
+// /          /         /
+// .          .         .
+// ..         .         ..
 
-dir(INODE *ip){
+pdir(INODE *tip){
+  struct stat nstat;
   char *buf[BLKSIZE];
-  int i;
+  int i, j;
   DIR *dp;
   char *cp;
-
-  printf("inumber rec_len path\n");
   for(i=0;i<=12;i++)
   {
-    if(ip->i_block[i]==0)
+    if(tip->i_block[i]==0)
       return 0;
-    get_block(dev, ip->i_block[i], buf);
+    get_block(dev, tip->i_block[i], buf);
     dp = (DIR *)buf;
     cp = buf;
     while(*cp){
-      printf("%5d ", dp->inode);
-      printf("%6d ", dp->rec_len);
-      printf("%6s\n", dp->name);
+      printf("%3d %6d %6s\n", dp->inode, dp->rec_len, dp->name);
       cp += dp->rec_len;
       dp = (DIR *)cp;
     }
@@ -59,9 +55,7 @@ dir(INODE *ip){
 u32 search(MINODE *mip, char *pathname){
   int n = 0, i = 0, j = 0;
   char *cp, sbuf[BLKSIZE];
-  INODE *ip;
   ip = &(mip->INODE);
-  printf("searching for %s...\n", pathname);
   for(i=0;i<12;i++){
     if(ip->i_block[i]==0)
       return 0;
@@ -69,18 +63,14 @@ u32 search(MINODE *mip, char *pathname){
     dp=(DIR *)sbuf;
     cp = sbuf;
     while(*cp){
-      printf("%5d ", dp->inode);
-      printf("%6d ", dp->rec_len);
-      printf("%6s\n", dp->name);
       for(j=0;j<strlen(pathname);j++){
         if(dp->name[j]==pathname[j]){
           if(j==strlen(pathname)-1){
-            printf("found %s at inumber %u\n", pathname, (u32)dp->inode);
             return dp->inode;
           }
         }
         else{
-            break;
+          break;
         }
       }
       cp += dp->rec_len;
@@ -90,8 +80,7 @@ u32 search(MINODE *mip, char *pathname){
   printf("search found nothing with the name %s\n", name);
   return 0;
 }
-u32 getino(int *dev, char *pathname){//returns inode # of a pathname.
-  //!!!!!!!!! probably doesnt follow a path of more than 1!!!!!!!but idk havent tested
+u32 getino(int *dev, char *pathname){//returns inode # of a pathname
   int n, i, ino, inostrt;
   if(pathname[0]=='/')
     dev = root->dev;
@@ -99,10 +88,10 @@ u32 getino(int *dev, char *pathname){//returns inode # of a pathname.
     dev = running->cwd->dev;
   n=tokenize(pathname);
   for(i=0;i<n;i++){
-    printf("search for '%s'\n", name[i]);
     ino = search(running->cwd->INODE, name[i]);
     if(ino==0)
       return 0;
+    //printf("found:%s at:%d\n", name[i], ino);
   }
   return ino;
 }
@@ -111,8 +100,6 @@ MINODE *iget(int dev, u32 ino){
   char buf[BLKSIZE];
   int i, blk, offset;
   MINODE *mip;
-  INODE *ip;
-
   //find inode on disk w/ mailmans
   blk=(ino-1)/8+mounttab[0].iblk;
   offset=(ino-1)%8;
@@ -134,17 +121,16 @@ MINODE *iget(int dev, u32 ino){
     if(mip->refCount==0){//take first available minode and init
       if(i==0){
         mip->INODE=ip;
+        printf("m%d i%d\n",mip->INODE->i_size, ip->i_size);
         mip->dev=dev;
         mip->ino=ino;
         mip->refCount=1;
         mip->dirty=0;
         mip->mounted=1;//flag if something is mounted on this minode
+        mip->locked=1;
         mip->mountptr=&mounttab[0];
         mounttab[0].mounted_inode=mip;
-        mip->next=0;
-        mip->parent=0;
-        mip->child=0;
-        mip->sibling=0;
+
       }
       mip->INODE=ip;
       mip->dev=dev;
@@ -152,24 +138,181 @@ MINODE *iget(int dev, u32 ino){
       mip->refCount=1;
       mip->dirty=0;
       mip->mounted=0;//flag if something is mounted on this minode
+      mip->locked=1;
       mip->mountptr=0;
-      mip->next=0;
-      mip->parent=0;
-      mip->child=0;
-      mip->sibling=0;
       return mip;
     }
   }
 }
 int iput(MINODE *mip){//release inode from memory
   int ino, blk, offset;
-  INODE *ip;
   char *buf[BLKSIZE];
-
   if(--mip->refCount==0&&mip->dirty){//if dirty and last reference in memory
+    mip->locked=0;
     memcpy(buf, mip->INODE, BLKSIZE);
     put_block(mip->dev, blk, buf);//write back
   }
   else if(mip->refCount>0||!mip->dirty)
     return 0;
+}
+int findname(MINODE *parent, int myino, char *myname){
+  int n = 0, i = 0, j = 0;
+  char *cp, buf[BLKSIZE];
+  ip = parent->INODE;
+  for(i=0;i<12;i++){
+    if(ip->i_block[i]==0)
+      return -1;
+    get_block(dev, ip->i_block[i], buf);
+    dp=(DIR *)buf;
+    cp = buf;
+    while(*cp){
+      if(dp->inode==myino){
+        if(myino==2){
+          strcpy(myname,"/");
+        }
+        else
+          strcpy(myname, dp->name);
+        // for(i=0;i<dp->name_len;i++){
+        //   printf("%c", dp->name[i]);
+        // }
+      }
+      cp += dp->rec_len;
+      dp = (DIR *)cp;
+    }
+  }
+  printf("search found nothing with the inode %s\n", myino);
+  return 0;
+}
+int findino(MINODE *mip, int *myino, int *parentino){
+  char *cp, buf[BLKSIZE];
+  ip=mip->INODE;
+  get_block(dev, ip->i_block[0], buf);
+  dp=(DIR *)buf;
+  cp=buf;
+  *myino=dp->inode;
+  //printf("%d\n",dp->inode);
+  cp+=dp->rec_len;
+  dp=(DIR *)cp;
+  *parentino=dp->inode;
+  //printf("%d\n",dp->inode);
+}
+listdir (char *pathname){
+  int i, ino = 0, dev = running->cwd->dev;
+  MINODE *mip = running->cwd;
+
+  if (pathname[0]!='\0'){   // ls pathname:
+    pathname[strlen(pathname)]='\0';
+    ino = getino(&dev, pathname);
+    if(ino)
+      mip = iget(dev, ino);
+  }
+  pdir(mip->INODE);
+  // mip points at minode;
+  // Each data block of mip->INODE contains entries
+  // print the name strings of the entries
+}
+ch_dir (char *pathname){
+  MINODE *mip;
+  int ino;
+  if(pathname[0]=='\0'||
+    (pathname[0]=='/'&&strlen(pathname)==1)){//cd to root;
+      dev = root->dev;
+      iput(running->cwd);
+      running->cwd=root;
+
+  }
+  else if(pathname[0]=='/'&&strlen(pathname)>1){
+    //go to root first
+    dev = root->dev;
+    iput(running->cwd);
+    running->cwd=root;
+    ino=getino(&dev, &pathname[1]);
+    if(ino)
+      running->cwd=iget(dev, ino);
+  }
+  else{//cd to pathname;
+    ino = getino(&dev, pathname);
+    //printf("switching to inode %d\n", ino);
+    if(ino){
+      running->cwd=iget(dev, ino);
+    }
+    else
+      printf("invalid pathname for cd\n");
+
+  }
+}
+pwd (MINODE *mip){
+  MINODE *parent;
+  char mname[128], temp[128], pname[128];
+  int myino=0, parentino=1;
+  memset(mname, '\0', 128);
+  memset(temp, '\0', 128);
+  memset(pname, '\0', 128);
+  while(1){
+    findino(mip, &myino, &parentino);
+    parent = iget(dev, parentino);
+    findname(parent, myino, mname);
+    strcpy(temp, mname);
+    strcat(temp, pname);
+    strcpy(pname, temp);
+    if(!strncmp(mname, "/", 1))
+      break;
+    mip=parent;
+  }
+  printf("%s\n", pname);
+}
+kmkdir(MINODE *pmip, char *path){
+  MINODE *mip;
+  int ino, blk, i;
+  // 5-1. allocate an INODE and a disk block:
+  ino = ialloc(dev);
+  blk = balloc(dev);
+  mip = iget(dev,ino); // load INODE into an minode
+  mip->INODE->i_block[0]=blk;
+  for(i=1;i<12;i++){mip->INODE->i_block[i]=0;}
+  mip->dirty=1;
+  iput(mip);
+  //make . and ..
+  //enter_child
+
+  // 5-2. initialize mip->INODE as a DIR INODE;
+  // mip ->INODE.i_block[0] = blk; other i_block[ ] are 0;
+  // mark minode modified (dirty);
+  // iput(mip); // write INODE back to disk
+  // 5-3. make data block 0 of INODE to contain . and .. entries;
+
+  // write to disk block blk.
+  // 5-4. enter_child(pmip, ino, basename); which enters
+  // (ino, basename) as a DIR entry to the parent INODE;
+}
+mk_dir(char *pathname){
+  char temp[128], *dname, *bname;
+  int pino, d=0, b=0;
+  MINODE *pmip;
+  strcpy(temp, pathname);
+  dname=dirname(temp);
+  bname=basename(pathname);
+  if(pathname[0]=='/'){
+    dev=root->dev;
+    if(!search(dname, root))
+      d=1;
+  }
+  else{
+    dev = running->cwd->dev;
+    if(!search(dname, running->cwd))
+      d=1;
+  }
+  if(d=0){
+    printf("dirname does not exist\n");
+    return 0;
+  }
+  pino=getino(&dev, dname);
+  pmip=iget(dev,dname);
+  if(search(pmip, bname)){
+    printf("basename already exists\n");
+    return 0;
+  }
+  kmkdir(pmip, bname);
+
+
 }
